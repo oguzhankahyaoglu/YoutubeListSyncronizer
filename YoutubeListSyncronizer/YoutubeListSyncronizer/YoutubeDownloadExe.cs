cf983f1f117343a3e47d1b9561de516ae094ef80
oguzhankahyaoglu <oguzhan.kahyaoglu@yahoo.com>
2016-08-15 13:27:45 +0300

    downloader görünüp görünmemesi configden ayarlanabilir durumda artık

--- YoutubeListSyncronizer/YoutubeListSyncronizer/YoutubeDownloadExe.cs
+++ YoutubeListSyncronizer/YoutubeListSyncronizer/YoutubeDownloadExe.cs
@@ -1,19 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Kahia.Common.Extensions.ConversionExtensions;
 using Kahia.Common.Extensions.StringExtensions;
 
 namespace YoutubeListSyncronizer
 {
     public static class YoutubeDownloadExe
     {
+        private static bool IsDownloaderVisible = ConfigurationManager.AppSettings["IsDownloaderVisible"].ToBoolean();
+
         #region Youtube-DL Exe
 
-        public static string DownloadVideoAndReturnsErrors(int index, string url, Form1.Args args, Action<String,int> onConsoleDataReceived)
+        public static string DownloadVideoAndReturnsErrors(int index, string url, Form1.Args args, Action<String, int> onConsoleDataReceived)
         {
             //"D:\Dropbox\Youtube Downloader\youtube-dl.exe" https://www.youtube.com/playlist?list=PLDZMiVQ0iUnCwGbMckmoupzrmTNRIo-Y0 -o "D:\_Videos\YT Favourites\%(autonumber)s-%(title)s.%(ext)s" --no-continue -w --playlist-reverse -i -f "mp4[height<=?720]"
             string[] processArgs =
@@ -34,7 +38,8 @@ namespace YoutubeListSyncronizer
             var process = new Process();
             process.StartInfo.FileName = ytExe.FullName;
             process.StartInfo.Arguments = processArgs.JoinWith(" ");
-            process.StartInfo.WindowStyle = ProcessWindowStyle.Minimized;
+            //process.StartInfo.WindowStyle = ProcessWindowStyle.Minimized;
+            process.StartInfo.WindowStyle = IsDownloaderVisible ? ProcessWindowStyle.Minimized : ProcessWindowStyle.Hidden;
             //process.StartInfo.UseShellExecute = false;
             //process.StartInfo.CreateNoWindow = false;
             //process.StartInfo.RedirectStandardOutput = false;
