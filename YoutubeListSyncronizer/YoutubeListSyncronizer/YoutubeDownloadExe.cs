b6b4119eb2ea0f1074f2b850ad56dfd21b5e2c4f
oguzhankahyaoglu <oguzhan.kahyaoglu@yahoo.com>
2019-09-10 23:21:13 +0300

    Tarih eklendi dosya isimlerinin başına, her zaman indirilen dosyanın en sonda görünebilmesi için

--- YoutubeListSyncronizer/YoutubeListSyncronizer/YoutubeDownloadExe.cs
+++ YoutubeListSyncronizer/YoutubeListSyncronizer/YoutubeDownloadExe.cs
@@ -19,12 +19,13 @@ namespace YoutubeListSyncronizer
 
         public static string DownloadVideoAndReturnsErrors(int index, string url, Form1.Args args, Action<String, int> onConsoleDataReceived)
         {
+            var now = DateTime.Now;
             //"D:\Dropbox\Youtube Downloader\youtube-dl.exe" https://www.youtube.com/playlist?list=PLDZMiVQ0iUnCwGbMckmoupzrmTNRIo-Y0 -o "D:\_Videos\YT Favourites\%(autonumber)s-%(title)s.%(ext)s" --no-continue -w --playlist-reverse -i -f "mp4[height<=?720]"
             string[] processArgs =
             {
                 url,
                 "--no-continue -w -i",
-                "-o \"{0}\\{1:D4}-%(title)s.%(ext)s\"".FormatString(args.VideoFolder,index+1),
+                $"-o \"{args.VideoFolder}\\{now:yyyyMM}-{(index+1):D4}-%(title)s.%(ext)s\"",
                 "-v ",
                 //"--encoding cp857",
                 //"--console-title",
